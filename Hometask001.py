f4b580e70e95d9604d111afb62e9da0bd6f1a577
EvgenyVarlamov <varlamov-el@yandex.ru>
2022-07-19 22:42:24 +0300

    add HomeTask #1 PI. Added: Bailey-Borwein-Plouffe

--- Hometask001.py
+++ Hometask001.py
@@ -2,9 +2,9 @@
 # # Пример:
 # # - при d = 3, π = 3.141
 
-n = int(input('Введите число от 4000: '))
+n = int(input('Введите число от 4000 до 40000: '))
 d = int(input('Введите точность округления: '))
-leibniz, madhava, basel = 0, 0, 0
+leibniz, madhava, basel, bbp = 0, 0, 0, 0
 c = 0
 for i in range(1, n):
     if i % 2 != 0:
@@ -12,6 +12,9 @@ for i in range(1, n):
         madhava += (-1)**c*1/(i*3**c)
         c += 1
     basel += 1/i**2
+    if i<257:
+        bbp += (4/(8*(i-1)+1)-2/(8*(i-1)+4)-1/(8*(i-1)+5)-1/(8*(i-1)+6))/16**(i-1)
 print(f'π = {round(leibniz, d)}')
 print(f'π = {round(madhava*12**0.5, d)}')
 print(f'π = {round((basel*6)**0.5, d)}')
+print(f'π = {round(bbp, d)}')
