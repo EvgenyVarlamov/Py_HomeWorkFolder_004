22ba6510acab76c0826d54c0bd6fb84e8e371a45
EvgenyVarlamov <varlamov-el@yandex.ru>
2022-07-20 09:55:47 +0300

    add HomeTask #3 List of non-repeating elements. Added: output by list(set())

--- Hometask003.py
+++ Hometask003.py
@@ -7,7 +7,7 @@ from random import randint as rnd
 def fill_array(arr):
     for i in range(len(arr)):
         arr[i] = rnd(0, len(arr))
-    print('Cписок всех элементов: ', arr)
+    print('Cписок всех элементов: ', '\n',  arr)
 
 
 def order(arr):
@@ -15,10 +15,11 @@ def order(arr):
     for i in range(len(arr)):
         if arr.count(arr[i]) == 1:
             num.append(arr[i])
-    print('Cписок неповторяющихся элементов: ', num)
+    print('Cписок неповторяющихся элементов: ', '\n',  num)
 
 
 n = int(input('Введите размер последовательности: '))
 numbers = ['']*n
 fill_array(numbers)
+print('Cписок однократно повторяющихся элементов: ', '\n', list(set(numbers)))
 order(numbers)
